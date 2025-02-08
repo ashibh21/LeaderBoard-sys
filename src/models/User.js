3bdf2bc8aac0348c7aa91c393bfbadd9393760d6
Ashish bhatt <98805495+ashibh21@users.noreply.github.com>
2025-02-08 19:20:09 +0530

    feat: added max 3 wrong login attempts and a cooldown period of 30 sec

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  countOfAttempts: { type: Number, default: 0 },
+  timestamp: { type: Date, default: Date.now },
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,18 +1,18 @@
-const router = require('express').Router();
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // User registration route
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
     return res
       .status(400)
-      .json({ message: 'Username and password are required' });
+      .json({ message: "Username and password are required" });
   }
 
   try {
@@ -20,7 +20,7 @@ router.post('/register', async (req, res) => {
     const existingUser = await User.findOne({ username });
 
     if (existingUser) {
-      return res.status(400).json({ message: 'User already exists' });
+      return res.status(400).json({ message: "User already exists" });
     }
 
     // Hash the password
@@ -30,45 +30,77 @@ router.post('/register', async (req, res) => {
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
 
-    res.status(201).json({ message: 'User registered successfully.' });
+    res.status(201).json({ message: "User registered successfully." });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).json({ message: "Server error", error });
   }
 });
 
-router.post('/login', async (req, res) => {
+router.post("/login", async (req, res) => {
   const { username, password } = req.body;
-
   if (!username || !password) {
     return res
       .status(400)
-      .json({ message: 'Username and password are required' });
+      .json({ message: "Username and password are required" });
   }
+  console.log("here3");
 
   try {
     // Find the user in MongoDB
-    const user = await User.findOne({ username });
+    const user = await User.findOne(
+      { username },
+      { password: 1, countOfAttempts: 1, timestamp: 1 } 
+    );
+    console.log("here2");
 
     if (!user) {
-      return res.status(400).json({ message: 'Invalid username or password.' });
+      return res.status(400).json({ message: "Invalid username or password." });
+    }
+
+    if (user.timestamp && user.countOfAttempts >= 2) {
+      const diff = Date.now() - user.timestamp;
+      if (diff < 30000) {
+        return res
+          .status(401)
+          .json({ message: "Account is locked. try after 30 sec" });
+      }
     }
 
     // Compare the hashed password
     const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(401).json({ message: 'Invalid username or password.' });
+      if (user.countOfAttempts >= 2) {
+        await User.findOneAndUpdate(
+          { username },
+          { $set: { timestamp: Date.now() } }
+        );
+        return res
+          .status(401)
+          .json({ message: "Account is locked. try after 30 sec" });
+      }
+      await User.findOneAndUpdate(
+        { username },
+        { $set: { countOfAttempts: user.countOfAttempts + 1 } } // Update timestamp in DB
+      );
+      return res.status(401).json({ message: "Invalid username or password." });
+    } else {
+      await User.findOneAndUpdate(
+        { username },
+        { $set: { countOfAttempts: 0 } } // Update timestamp in DB
+      );
     }
 
     // Generate a JWT token
     const token = jwt.sign({ username: user.username }, JWT_SECRET, {
-      expiresIn: '1h',
+      expiresIn: "1h",
     });
 
-    res.status(200).json({ message: 'Login successful', token });
+    res.status(200).json({ message: "Login successful", token });
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    console.log(error);
+    res.status(500).json({ message: "Server error", error });
   }
 });
 
